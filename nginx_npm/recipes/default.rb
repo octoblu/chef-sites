088111b8de0e02da3fbe30a6b40f85a2a9c1adb2
Jade Meskill <jade@integrumtech.com>
2014-07-22 22:38:43 -0700

    mkdir just in case

--- nginx_npm/recipes/default.rb
+++ nginx_npm/recipes/default.rb
@@ -13,7 +13,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "/tmp"
     code <<-EOH
-      sudo su - #{deploy[:user]} -c 'if [ -f #{deploy[:deploy_to]}/current ]; then ln -s #{deploy[:deploy_to]}/shared/node_modules #{deploy[:deploy_to]}/current; cd #{deploy[:deploy_to]}/current && npm install --dev; fi' 2>&1
+      sudo su - #{deploy[:user]} -c 'if [ -f #{deploy[:deploy_to]}/current ]; then mkdir -p #{deploy[:deploy_to]}/shared/node_modules; ln -s #{deploy[:deploy_to]}/shared/node_modules #{deploy[:deploy_to]}/current; cd #{deploy[:deploy_to]}/current && npm install --dev; fi' 2>&1
     EOH
   end
 end
