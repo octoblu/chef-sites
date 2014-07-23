288c6038e6a8e815995d173ea0798cb30526452f
Jade Meskill <jade@integrumtech.com>
2014-07-22 21:49:35 -0700

    try running in a full shell

--- nginx_npm/recipes/default.rb
+++ nginx_npm/recipes/default.rb
@@ -1,23 +1,3 @@
-#
-# Cookbook Name:: nginx
-# Recipe:: default
-# Author:: AJ Christensen <aj@junglist.gen.nz>
-#
-# Copyright 2008, OpsCode, Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
 script "npm install npm" do
   interpreter "bash"
   user "root"
@@ -30,10 +10,10 @@ end
 node[:deploy].each do |application, deploy|
   script "npm install" do
     interpreter "bash"
-    user deploy[:user]
-    cwd "#{deploy[:deploy_to]}/current"
+    user "root"
+    cwd "/tmp"
     code <<-EOH
-      npm install
+      sudo su - #{deploy[:user]} -c 'cd #{deploy[:deploy_to]}/current && npm install' 2>&1
     EOH
   end
 end
