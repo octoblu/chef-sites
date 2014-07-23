212ade56195d4ad0b35c096d67ca8e431302995b
Jade Meskill <jade@integrumtech.com>
2014-07-22 22:09:40 -0700

    symlink

--- nginx_npm/recipes/default.rb
+++ nginx_npm/recipes/default.rb
@@ -13,7 +13,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "/tmp"
     code <<-EOH
-      sudo su - #{deploy[:user]} -c 'if [ -f #{deploy[:deploy_to]}/current ]; then cd #{deploy[:deploy_to]}/current && npm install; fi' 2>&1
+      sudo su - #{deploy[:user]} -c 'if [ -f #{deploy[:deploy_to]}/current ]; then ln -s #{deploy[:deploy_to]}/shared/node_modules #{deploy[:deploy_to]}/current/node_modules && cd #{deploy[:deploy_to]}/current && npm install; fi' 2>&1
     EOH
   end
 end
