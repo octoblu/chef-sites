ac66fe9bad8c7348b007b9c248d3f36893af7381
Jade Meskill <jade@integrumtech.com>
2014-07-22 22:02:35 -0700

    make sure dir exists

--- nginx_npm/recipes/default.rb
+++ nginx_npm/recipes/default.rb
@@ -13,7 +13,7 @@ node[:deploy].each do |application, deploy|
     user "root"
     cwd "/tmp"
     code <<-EOH
-      sudo su - #{deploy[:user]} -c 'cd #{deploy[:deploy_to]}/current && npm install' 2>&1
+      sudo su - #{deploy[:user]} -c 'if [ -f #{deploy[:deploy_to]}/current ]; then cd #{deploy[:deploy_to]}/current && npm install; fi' 2>&1
     EOH
   end
 end
